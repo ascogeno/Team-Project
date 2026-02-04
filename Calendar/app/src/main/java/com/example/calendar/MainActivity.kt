758ce532bf50531406042fc6ba752e584c87f965
kayla133 <makaylahansen04@gmail.com>
2026-02-04 11:00:13 -0700

    Presentation

--- Calendar/app/src/main/java/com/example/calendar/MainActivity.kt
+++ Calendar/app/src/main/java/com/example/calendar/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.calendar.ui.theme.CalendarTheme
